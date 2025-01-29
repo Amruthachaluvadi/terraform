d23d205e492b7445c9eaa1bf69008fdcf5d8ee1f
Amruthachaluvadi <tdeveloper243@gmail.com>
2025-01-30 00:42:40 +0530

    terraform-day1

---
+++ ec2.tf
@@ -0,0 +1,37 @@
+ resource "aws_instance" "web" {
+   ami           = "ami-09c813fb71547fc4f"
+   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type = "t3.micro"
+
+   tags = {
+     Name = "HelloWorld"
+   }
+ }
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+ # vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]   #from all server u are alloing only port 22 
+
+  } 
+
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
